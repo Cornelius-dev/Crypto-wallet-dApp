599ba240b9d6fbde6f74af62a900c9b0f825b6a4
Cornelius Goodness Olubunmi <120067321+Cornelius-dev@users.noreply.github.com>
2025-05-04 13:41:03 +0100

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Crypto Wallet DApp</h2>
+      <p>This is a sample frontend for sending ETH and checking balance using Ethers.js and MetaMask.</p>
+    </div>
+  );
+}
+
+export default App;
